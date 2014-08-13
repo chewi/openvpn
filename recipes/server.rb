14c059c14c99a1b0365823f8901a7dd9b764a5b2
James Le Cuirot <james.le-cuirot@yakara.com>
2014-08-13 12:22:13 +0100

    License cleanup.

--- recipes/server.rb
+++ recipes/server.rb
@@ -1,6 +1,21 @@
-# routes = node['openvpn']['routes']
-# routes << node['openvpn']['push'] if node['openvpn'].attribute?('push')
-# node.default['openvpn']['routes'] = routes.flatten
+#
+# Cookbook Name:: openvpn
+# Recipe:: server
+#
+# Copyright 2014, Xhost Australia
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
 # in the case the key size is provided as string, no integer support in metadata (CHEF-4075)
 node.override['openvpn']['key']['size'] = node['openvpn']['key']['size'].to_i
